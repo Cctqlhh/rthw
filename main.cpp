6060f2a039e019ad4c377ceda985e9006576f530
Cctqlhh <550160403@qq.com>
2024-03-07 22:09:16 +0800

    tiaosh

--- main.cpp
+++ main.cpp
@@ -87,15 +87,27 @@ int main()
     {
         int id = Input(); // 读取场面信息 id第几帧
         
-        for(int i = 0; i < robot_num; i ++)
+        for(int i = 0; i < robot_num; i ++){
+        
         // 机器人指令
-            printf("move %d %d\n", i, rand() % 4); //指令：move 机器人id0-9  移动方向左右上下0123
+            if(robot[i].status == 1){
+                printf("move %d %d\n", i, 1);
+            // printf("move %d %d\n", i, rand() % 4); //指令：move 机器人id0-9  移动方向右左上下0123
+                if(robot[i].goods == 0)
+                    printf("get %d\n", i); //指令：get 机器人id0-9  取货
+                else
+                    printf("put %d\n", i); //指令：put 机器人id0-9  放货
+            }
+                
+            // printf("move %d %d\n", 3, 1);
+            // printf("move %d %d\n", 7, 1);
+
             // get/pull id；货物生成处未携带物品则取货成功/泊位处且携带物品则放置成功
         //船指令
             // ship id0-4 泊位id0-9 船移动到泊位
             // go id0-4 船运输货物到虚拟点
-
-
+        }
+        
         puts("OK"); // 所有指令结束后OK
         fflush(stdout);
     }
