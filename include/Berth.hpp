c94344d41a5b629211e6684e35a0887ad0a83708
liuqingzhouzhou <1764025709@qq.com>
2024-03-17 13:30:12 +0800

    泊位选物品代码部分完善（待续）

--- include/Berth.hpp
+++ include/Berth.hpp
@@ -35,10 +35,10 @@ struct Berth
         auto curframe_it = cur_things.begin();  // 当前帧最近物品迭代器
         if(cur_things.size() != 0)
         {
-            //遍历当前帧中的物品对象
+            //遍历当前帧中的物品对象，选出当前帧最近的物品
             for (auto it = cur_things.begin(); it != cur_things.end(); ++it)
             {
-                distance = abs(this->x - it->x) + abs(this->y - it->y);
+                distance = abs(this->x - it->x) + abs(this->y - it->y); // 修改为曼哈顿距离函数！！！！！！！！！！！
                 it->dst_to_breth = distance;
                 //选出当前帧的最近物品
                 if (distance < curframe_min_distance)  //如果当前帧物品距离比历史最近物品更近，则更新
@@ -60,23 +60,22 @@ struct Berth
                 cur_things.erase(curframe_it);   //删除当前帧最近物品
             }
         }
-    }
 
-    void changeGoal(int curframe_id, int status)//当前帧id，机器人状态
-    {
+        // 每一帧一定要运行此函数，不管当前泊位检测到的当前帧物品是否为空
+        // 每一帧更新完毕，判断是否超时，超时则删除map中的物品
         // 对map容器的每帧最近物品进行判断，如果超时，则删除
         for (auto it = things_map.begin(); it != things_map.end(); ++it)
         {
-            if((1000-(curframe_id - it->first)) <= 300)
+            if((1000-(cur_things[0].frame_id - it->first)) <= 300)
             {
                 things_map.erase(it);
             }
         }
-        // 2.如果历史最近物品超时或者机器人取走了历史最近物品，则根据map容器强行更新历史最近物品
-        if((1000-(curframe_id - nearest_thing.frame_id)) <= 300)
+        // 如果历史最近物品超时，则根据map容器强行更新历史最近物品
+        if((1000-(cur_things[0].frame_id - nearest_thing.frame_id)) <= 300)
         {
-            int cur_min_distance = INT_MAX;
-            Things cur_nearest_thing;
+            int cur_min_distance = INT_MAX;            // 当前最近物品的距离(用于遍历map容器)
+            Things cur_nearest_thing;                  // 当前最近物品(用于遍历map容器)
             for (auto it = things_map.begin(); it != things_map.end(); ++it)
             {
                 if (it->second.dst_to_breth < cur_min_distance)
@@ -88,31 +87,81 @@ struct Berth
             this->min_distance = cur_min_distance;
             this->nearest_thing = cur_nearest_thing;
         }
+    }
 
-        //2.机器人取走历史最近物品，重新更新历史最近物品
-        //如何判断机器人取走了历史最近物品？要根据机器人的状态吗，还是机器人的坐标
-        if(status == 1)
-        {//机器人取走了历史最近物品，更新历史最近物品
-            things_map.erase(this->nearest_thing.frame_id);  //先将历史最近物品从map中删除
+     
 
-            //每次都要遍历things_map，这里可以单独写一个遍历函数，减少重复代码
-            int cur_min_distance = INT_MAX;
-            Things cur_nearest_thing;
-            for (auto it = things_map.begin(); it != things_map.end(); ++it)
+    // void changeGoal(int curframe_id, int status)//当前帧id，机器人状态
+    // {
+    //     // 对map容器的每帧最近物品进行判断，如果超时，则删除
+    //     for (auto it = things_map.begin(); it != things_map.end(); ++it)
+    //     {
+    //         if((1000-(curframe_id - it->first)) <= 300)
+    //         {
+    //             things_map.erase(it);
+    //         }
+    //     }
+    //     // 2.如果历史最近物品超时，则根据map容器强行更新历史最近物品
+    //     if((1000-(curframe_id - nearest_thing.frame_id)) <= 300)
+    //     {
+    //         int cur_min_distance = INT_MAX;
+    //         Things cur_nearest_thing;
+    //         for (auto it = things_map.begin(); it != things_map.end(); ++it)
+    //         {
+    //             if (it->second.dst_to_breth < cur_min_distance)
+    //             {
+    //                 cur_min_distance = it->second.dst_to_breth;
+    //                 cur_nearest_thing = it->second;
+    //             }
+    //         }
+    //         this->min_distance = cur_min_distance;
+    //         this->nearest_thing = cur_nearest_thing;
+    //     }
+
+    //     //2.机器人取走历史最近物品，重新更新历史最近物品
+    //     //如何判断机器人取走了历史最近物品？要根据机器人的状态吗，还是机器人的坐标
+    //     if(status == 1)
+    //     {//机器人取走了历史最近物品，更新历史最近物品
+    //         things_map.erase(this->nearest_thing.frame_id);  //先将历史最近物品从map中删除
+
+    //         //每次都要遍历things_map，这里可以单独写一个遍历函数，减少重复代码
+    //         int cur_min_distance = INT_MAX;
+    //         Things cur_nearest_thing;
+    //         for (auto it = things_map.begin(); it != things_map.end(); ++it)
+    //         {
+    //             if (it->second.dst_to_breth < cur_min_distance)
+    //             {
+    //                 cur_min_distance = it->second.dst_to_breth;
+    //                 cur_nearest_thing = it->second;
+    //             }
+    //         }
+    //         this->min_distance = cur_min_distance;
+    //         this->nearest_thing = cur_nearest_thing;
+
+    //     }
+    // }
+
+    // 机器人取走历史最近物品，需要根据每个泊位的每帧最近物品map，重新更新历史最近物品
+    void update_nearest_thing_from_history()
+    {
+        int cur_min_distance = INT_MAX;
+        Things cur_nearest_thing;
+
+        things_map.erase(this->nearest_thing.frame_id);  //先将历史最近物品从map中删除
+
+        //每次都要遍历things_map，这里可以单独写一个遍历函数，减少重复代码
+        int cur_min_distance = INT_MAX;
+        Things cur_nearest_thing;
+        for (auto it = things_map.begin(); it != things_map.end(); ++it)
+        {
+            if (it->second.dst_to_breth < cur_min_distance)
             {
-                if (it->second.dst_to_breth < cur_min_distance)
-                {
-                    cur_min_distance = it->second.dst_to_breth;
-                    cur_nearest_thing = it->second;
-                }
+                cur_min_distance = it->second.dst_to_breth;
+                cur_nearest_thing = it->second;
             }
-            this->min_distance = cur_min_distance;
-            this->nearest_thing = cur_nearest_thing;
-
         }
-
-
-
+        this->min_distance = cur_min_distance;
+        this->nearest_thing = cur_nearest_thing;
     }
 
 };
